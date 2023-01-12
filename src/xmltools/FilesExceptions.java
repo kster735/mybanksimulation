6b9e34dc3fc5e97cd2451a6614103d81e659f11f
stergios <stergios.katsogiannis@gmail.com>
2023-01-12 02:49:21 +0200

    Fixed all issues with the XML parser. Now I only need to write a function to actually convert the Hashtable to an actual object.

--- src/xmltools/FilesExceptions.java
+++ src/xmltools/FilesExceptions.java
@@ -24,11 +24,14 @@ public class FilesExceptions {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        Person takis = new Person("Makis", "Makis", 78);
+//        Person takis = new Person("Takis", "Takis", 78);
 //        Person lakis = new Person("Lakis", "Lakis", 56);
+//        Person makis = new Person("Makis", "Makis", 43);
+//
 //        ArrayList<Person> persons = new ArrayList<>();
 //        persons.add(takis);
 //        persons.add(lakis);
+//        persons.add(makis);
 //        
 //        File f = new File("Persons.xml");
 //        
@@ -47,13 +50,8 @@ public class FilesExceptions {
             fis.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
-        } finally {
-        }
+        } 
         
-//        for (Person p: persons){
-//            System.out.println(p.getFirstName() + " "+ p.getLastName() + " " + p.getAge() );
-//        }
-
         System.out.println(myobjects);
     }
     
@@ -72,44 +70,40 @@ public class FilesExceptions {
         Hashtable<String, Hashtable<String, String>> objects = new Hashtable<String, Hashtable<String, String>>();
         Hashtable<String, String> tempObject = new Hashtable<String, String>();
 
+        // Counter is needed to store multiple objects of the same type
         int objectCounter=0;
         
         while (fis.hasNextLine()){
             boolean hasText = false;
             // Store next line of xml to process
-            line = fis.nextLine();
+            line = fis.nextLine().trim();
             
             // Don't do anything with xml heading
             if (line.startsWith("<?")){
                 continue;
             }
             
-            // if line starts with closing tag
+            // if line starts with closing tag store the object.
             if (line.startsWith("</")){
                 closingTag = line;
-                objectType = closingTag.substring(2, closingTag.length() - 1) + objectCounter++;
-
-                objects.put(objectType, tempObject);
+                objectType = closingTag.substring(2, closingTag.length() - 1) + objectCounter++;                
+                objects.put(objectType, (Hashtable<String,String>) tempObject.clone());
+                tempObject.clear();
                 continue;
             }
                 
-            
             // Find and store next openning tag and it's content and store them
             int i = line.indexOf(">");
             openningTag = line.substring(0,i+1);
             content = line.substring(i+1);
             
-            // Find and store text if the current xml element has one and store the 
-            // closing tag too.
+            // Find and store text if the current xml element has one.
             int j = content.indexOf("</");
             if (j !=-1) {                
                 text = content.substring(0,j);
-
-                closingTag = content.substring(j);
-                element = openningTag.substring(2,openningTag.length()-1);
-                
-                tempObject.put(openningTag, text);
-                
+                element = openningTag.substring(1,openningTag.length()-1);
+                // build the the object by storing the field element and its value to a temp Hashtable
+                tempObject.put(element, text);
             }
             
         }
